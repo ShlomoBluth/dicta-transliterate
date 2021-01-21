cefab57075a895da70c8936cdbc9815338fa85ca
Shlomo Bluth <lomibluth@gmail.com>
2021-01-21 06:49:47 +0200

    add commit

---
+++ cypress/integration/requestsTests.js
@@ -0,0 +1,31 @@
+/// <reference types="cypress"/>
+
+//run tests on requests from nakdan run
+
+describe('requestsTests',()=>{
+
+    
+  beforeEach(() => {
+    cy.visit('https://transliterate.dicta.org.il/')
+  })
+
+
+  it('Error message for api response with a delay of 3 minutes when',()=>{
+    cy.transliterateRequest({
+      url:'api',
+      message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
+      delaySeconds:60*5
+    })
+  })
+
+  
+
+  it('Error message for api response with status code 500',()=>{
+    cy.transliterateRequest({
+      url:'api',
+      status:500,
+      message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
+    })
+  })
+    
+})
---
+++ cypress/support/commands.js
@@ -0,0 +1,25 @@
+
+
+
+Cypress.Commands.add('transliterateRequest',({url,status=200,message='',delaySeconds=0})=>{
+  cy.intercept( 'POST', '**/'+url, {
+    delayMs:1000*delaySeconds,
+    statusCode: status
+  })
+  if(message.length>0){
+    cy.contains(message).should('not.exist')
+  }
+  cy.get('[placeholder="הזן טקסט כאן"]').type('מ')
+
+  if(delaySeconds>0){
+    cy.get('[class*="spinner"]',{timeout:1000*delaySeconds}).should('not.exist')
+  }else{
+    cy.get('[class*="spinner"]').should('not.exist')
+  }
+  
+  if(message.length>0){
+    cy.contains(message).should('exist')
+  }
+})   
+  
+ 
