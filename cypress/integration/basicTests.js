adbf489c50eeec8a4139953400a446f0e8d219e4
Shlomo Bluth <lomibluth@gmail.com>
2021-05-03 16:35:30 +0300

    add commit

--- cypress/integration/basicTests.js
+++ cypress/integration/basicTests.js
@@ -2,19 +2,37 @@
 
 //run basic tests on transliterate
 
-describe('basicTests',()=>{
+let sizes = ['iphone-x',[1000, 660]]
 
-    before(() => {
-        cy.visit('https://transliterate.dicta.org.il/')
-    })
+sizes.forEach((size) => {
+
+    describe('basicTests',()=>{
+
+        before(() => {
+            if (Cypress._.isArray(size)) {
+                Cypress.config({
+                    viewportWidth: size[0],
+                    viewportHeight: size[1]
+                })
+                cy.viewport(size[0], size[1])
+            } else {
+                Cypress.config({
+                    viewportWidth: 375,
+                    viewportHeight: 812
+                })
+                cy.viewport(size)
+            }
+            cy.visit('/')
+        })
+        
     
-
-    it('transliterateRun',()=>{
-        cy.get('[placeholder="הזן טקסט כאן"]').type('תּוֹרָה')
-        cy.get('[class*="spinner"]').should('not.exist')
-        cy.get('.results-content').should('contain','torah')
-    })
-
+        it('transliterateRun',()=>{
+            cy.get('[placeholder="הזן טקסט כאן"]').type('תּוֹרָה')
+            cy.get('[class*="spinner"]').should('not.exist')
+            cy.get('.results-content').should('contain','torah')
+        })
     
+           
+    })
 
 })
--- cypress/integration/requestsTests.js
+++ cypress/integration/requestsTests.js
@@ -2,30 +2,50 @@
 
 //run tests on requests from transliterate run
 
-describe('requestsTests',()=>{
+let sizes = ['iphone-x',[1000, 660]]
+
+sizes.forEach((size) => {
+
+  describe('requestsTests',()=>{
 
     
-  beforeEach(() => {
-    cy.visit('https://transliterate.dicta.org.il/')
-  })
-
-
-  it('Error message for api response with a delay of 3 minutes when',()=>{
-    cy.transliterateRequest({
-      url:'api',
-      message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
-      delaySeconds:60*5
+    beforeEach(() => {
+      if (Cypress._.isArray(size)) {
+        Cypress.config({
+          viewportWidth: size[0],
+          viewportHeight: size[1]
+        })
+        cy.viewport(size[0], size[1])
+      } else {
+        Cypress.config({
+          viewportWidth: 375,
+          viewportHeight: 812
+        })
+        cy.viewport(size)
+      }
+      cy.visit('/')
     })
-  })
-
   
-
-  it('Error message for api response with status code 500',()=>{
-    cy.transliterateRequest({
-      url:'api',
-      status:500,
-      message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
+  
+    it('Error message for api response with a delay of 3 minutes when',()=>{
+      cy.transliterateRequest({
+        url:'api',
+        message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר',
+        delaySeconds:60*5
+      })
     })
-  })
+  
     
+  
+    it('Error message for api response with status code 500',()=>{
+      cy.transliterateRequest({
+        url:'api',
+        status:500,
+        message:'אופס יש לנו בעיה נסו שנית, או בקרו באתר מאוחר יותר'
+      })
+    })
+      
+  })
+
 })
+
